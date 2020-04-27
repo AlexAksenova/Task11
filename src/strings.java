c8ffe7a5de336be5e306dac6fb9c89064e2f32f7
AlexAksenova <saneshka916@gmail.com>
2020-04-27 17:32:54 +0300

    Initial commit

---
+++ src/strings.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class strings
+{
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите фразу: ");
+        String phrase = scanner.nextLine();
+        System.out.println(phrase);
+    }
+
+}
